08560f3718c3ac6539e5679e8f11f7cd903b9c77
Ahmetzyanov <prorain88@gmail.com>
2024-11-23 18:00:00 +0500

    add: utils package and ConnectionManager

---
+++ src/main/java/ru/exchange/utils/ConnectionManager.java
@@ -0,0 +1,31 @@
+package ru.exchange.utils;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    //private static final ConnectionManager CONNECTION_MANAGER = new ConnectionManager();
+
+    private static HikariDataSource dataSource;
+
+    static {
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:sqlite:C:/apache-tomcat-11.0.0/apache-tomcat-11.0.0/bin/exchange.db");
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+
+    }
+
+    private ConnectionManager() {
+
+    };
+
+    public static Connection getConnection(){
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
